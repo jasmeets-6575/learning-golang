41d545f5b25dbe7a99a9f42bcacbc21bba133575
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-10-18 21:28:17 +0530

    uint float

--- by-hitesh-chaudhary/02-varTypesConst/main.go
+++ by-hitesh-chaudhary/02-varTypesConst/main.go
@@ -10,4 +10,12 @@ func main() {
 	var isLoggedIn bool = true
 	fmt.Println(isLoggedIn)
 	fmt.Printf("Variable is of type: %T \n", isLoggedIn)
+	
+	var smallVal uint8 = 255
+	fmt.Println(smallVal)
+	fmt.Printf("Variable is of type: %T \n", smallVal)
+	
+	var smallFloat float64 = 255.232341412412412345667
+	fmt.Println(smallFloat)
+	fmt.Printf("Variable is of type: %T \n", smallFloat)
 }
