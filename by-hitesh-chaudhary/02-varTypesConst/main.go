b76ab66f0bd5a939b9e6ea1eb98ecb4c4a7d4d66
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-10-18 21:17:28 +0530

    variables

---
+++ by-hitesh-chaudhary/02-varTypesConst/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var username string = "Jasmeet"
+	fmt.Println(username)
+	fmt.Printf("Variable is of type: %T \n", username)
+
+	var isLoggedIn bool = true
+	fmt.Println(isLoggedIn)
+	fmt.Printf("Variable is of type: %T \n", isLoggedIn)
+}
